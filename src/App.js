a619f0aa8d61f6840852975c809ca4d784dc1ba5
VladFearo <69718459+VladFearo@users.noreply.github.com>
2023-11-09 13:36:51 +0200

    Added github API and search functionality

--- src/App.js
+++ src/App.js
@@ -4,25 +4,28 @@ import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import { GithubProvider } from "./context/github/GithubContext";
 
 
 export default function App ()
 {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
-        <main className="container mx-auto px-3" >
-          <Routes>
-            <Route path="/" element={ <Home /> } />
-            <Route path="/about" element={ <About /> } />
-            <Route path="/notfound" element={ <NotFound /> } />
-            <Route path="/*" element={ <NotFound /> } />
+    <GithubProvider >
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
+          <main className="container mx-auto px-3" >
+            <Routes>
+              <Route path="/" element={ <Home /> } />
+              <Route path="/about" element={ <About /> } />
+              <Route path="/notfound" element={ <NotFound /> } />
+              <Route path="/*" element={ <NotFound /> } />
 
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
